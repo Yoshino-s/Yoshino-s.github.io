e8f7fb52bd7e5a5e577c4fce304c8ec1a46ae484
yoshino-s <904814779@qq.com>
2018-04-14 20:33:44 +0800

    add device page

--- public/js/main.js
+++ public/js/main.js
@@ -7,11 +7,12 @@ requirejs.config({
 		muse: "https://unpkg.com/muse-ui/dist/muse-ui",
 		axios: "https://unpkg.com/axios/dist/axios.min",
 		sortablejs: "https://cdn.jsdelivr.net/npm/sortablejs@1.7.0/Sortable.min",
-		dragable: "https://cdnjs.cloudflare.com/ajax/libs/Vue.Draggable/2.16.0/vuedraggable.min"
+		dragable: "https://cdnjs.cloudflare.com/ajax/libs/Vue.Draggable/2.16.0/vuedraggable.min",
+		echarts: "https://cdnjs.cloudflare.com/ajax/libs/echarts/4.0.4/echarts"
 	}
 });
 console.timeStamp("requirejs loaded", true);
-requirejs(["polyfill", "vue", "vue-router", "vconsole", "muse", "component" ,"utils"], function(pf, Vue, Router, VConsole, Muse, cp, utils) {
+requirejs(["polyfill", "vue", "vue-router", "vconsole", "muse", "component" ,"utils", "echarts"], function(pf, Vue, Router, VConsole, Muse, cp, utils, echarts) {
 	console.timeStamp("main start", true);
 	let vc = new VConsole();
 	Vue.use(Router);
@@ -37,6 +38,9 @@ requirejs(["polyfill", "vue", "vue-router", "vconsole", "muse", "component" ,"ut
 			}, {
 				path: '/adddevice',
 				component: {template: "<add-device/>"}
+			}, {
+				path: '/device/:id',
+				component: {template: "<device/>"}
 			}, {
 				path: '*',
 				component: c.error
@@ -65,4 +69,59 @@ requirejs(["polyfill", "vue", "vue-router", "vconsole", "muse", "component" ,"ut
 	}).catch(e=>{
 		console.log(e.message)
 	});
+	/*var myChart = echarts.init(document.getElementById('ttt'));
+
+        // 指定图表的配置项和数据
+        var option = {
+            title: {
+                text: 'ECharts 入门示例'
+            },
+            legend: {
+                data:['alpha',"beta", "gamma"]
+            },
+            xAxis: {
+            	data: []
+			},
+			yAxis: {
+				type: "value",
+				min: -500,
+				max: 500
+			},
+            series: [{
+            	name: "alpha",
+                type: 'line',
+                data: []
+            },{
+            	name: "beta",
+                type: 'line',
+                data: []
+            },{
+            	name: "gamma",
+                type: 'line',
+                data: []
+            }]
+        };
+    let alpha = 0, beta = 0, gamma = 0;
+	window.addEventListener('devicemotion', event=>{
+		let interval = event.interval
+		alpha+=event.rotationRate.alpha*interval/1000;
+		alpha%=360;
+		beta+=event.rotationRate.beta*interval/1000;
+		beta%=360;
+		gamma+=event.rotationRate.gamma*interval/1000;
+		gamma%=360;
+	});
+	let addAlpha = ()=>{
+		option.series[0].data.push(alpha);
+		option.series[1].data.push(beta);
+		option.series[2].data.push(gamma);
+		if(option.series[0].data.length>50) {
+			option.series[0].data.shift(0);
+			option.series[1].data.shift(0);
+			option.series[2].data.shift(0);
+		}
+		myChart.setOption(option);
+		setTimeout(addAlpha, 100);
+	}
+	setTimeout(addAlpha, 100);*/
 });
